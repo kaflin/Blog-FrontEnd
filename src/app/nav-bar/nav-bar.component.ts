05c03afef68fc579668a7ecac6be71f730685bc5
suraj_gupta <suraj520876@gmail.com>
2021-03-14 21:29:02 +0545

    added navbar and body

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,22 @@
+import {Component, HostListener, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+  // sidenav: any;
+  isSticky: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.isSticky = window.pageYOffset >= 250;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
